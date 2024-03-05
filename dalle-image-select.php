97784f84cc4c4914bd1b0631ecbc872a0f838744
kemco2019 <128669621+kemco2019@users.noreply.github.com>
2024-03-05 15:01:55 +0900

    Update dalle-image-select.php

--- dalle-image-select.php
+++ dalle-image-select.php
@@ -100,5 +100,5 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="audio.js"></script>
-    <script type="text/javascript" src="dall-test.js"></script>
+    <script type="text/javascript" src="js/audio.js"></script>
+    <script type="text/javascript" src="js/dall-test.js"></script>
