38276c83af8917c57161071a3f8841b10c60c5df
kemco2019 <128669621+kemco2019@users.noreply.github.com>
2024-03-05 15:36:49 +0900

    Delete dalle-save.js

--- dalle-save.js
+++
@@ -1,6 +0,0 @@
-const fetchForm = document.querySelector('.fetchForm');
-const btn = document.querySelector('.btn');
-const url = 'https://studio.kemco.keio.ac.jp/ukiyoe2023/test.php';
-//const file = makeFile("image/dalle.png");// imgデータをfileに変換する処理
-//console.log(file);
-const csvFile = new File([csvBlob], 'ダミーファイル名.csv', {type: "text/plain"});
